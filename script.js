718dc210ccf11edf8cf7ed7f0fe05564a520aab4
Mithlesh <mithleshji2021@gmail.com>
2024-09-06 15:40:43 +0530

    add all files of calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("display");
+
+function appendCharacter(char) {
+    display.value += char;
+    
+}
+
+display.addEventListener("keydown",function(event){
+    event.preventDefault();
+})
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.vlue = "Error";
+    }
+}
